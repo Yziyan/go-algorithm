d5ac2ddd8d5c0663d970444eaff5edd72b6c6094
Ciusyan <1533460130@qq.com>
2023-07-23 21:43:45 +0800

    交替打印字符串和数字

---
+++ other/other_test.go
@@ -0,0 +1,12 @@
+// @Author: Ciusyan 2023/7/23
+
+package other_test
+
+import (
+	"github.com/Yziyan/go-algorithm/other"
+	"testing"
+)
+
+func TestPrint(t *testing.T) {
+	other.Print()
+}
---
+++ other/交替打印字符串和数字.go
@@ -0,0 +1,47 @@
+// @Author: Ciusyan 2023/7/23
+
+package other
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 最终实现的效果形如：AB12CD34EF......
+func printNum(ch chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 1; i <= 26; i++ {
+		fmt.Print(i)
+		// 通知另一个协程打印字符串
+		ch <- struct{}{}
+
+		// 等待另一个协程通知了再继续打印下一个数字
+		<-ch
+	}
+}
+
+func printStr(ch chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 'A'; i <= 'Z'; i++ {
+		// 等待通知了再打印
+		<-ch
+		fmt.Printf("%c", i)
+
+		// 通知另一个协程打印数字
+		ch <- struct{}{}
+	}
+}
+
+func Print() {
+	ch := make(chan struct{}, 1)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go printNum(ch, &wg)
+	go printStr(ch, &wg)
+
+	wg.Wait()
+}
