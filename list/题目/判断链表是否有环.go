94b5385742bccc0f42f45187e1a87ce8532cf6dd
Ciusyan <1533460130@qq.com>
2023-07-26 19:31:03 +0800

    判断链表是否有环

---
+++ list/题目/判断链表是否有环.go
@@ -0,0 +1,25 @@
+// @Author: Ciusyan 2023/7/26
+
+package 题目
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	var (
+		slow = head
+		fast = head.Next
+	)
+
+	for fast != nil && fast.Next != nil {
+		if slow == fast {
+			return true
+		}
+
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return false
+}
