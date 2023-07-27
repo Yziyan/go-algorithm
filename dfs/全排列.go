b257d1944f247fe6271588589c1efe39e0218724
Ciusyan <1533460130@qq.com>
2023-07-27 09:48:35 +0800

    全排列

--- dfs/全排列.go
+++ dfs/全排列.go
@@ -8,16 +8,49 @@ func Permute(nums []int) [][]int {
 	if nums == nil {
 		return nil
 	}
-	l := len(nums)
-	track := make([]int, l)
-	used := make([]bool, l)
+
 	results := make([][]int, 0)
-	dfs(0, &results, nums, track, used)
+	dfs(0, nums, &results)
 
 	return results
 }
 
-func dfs(level int, results *[][]int, nums, track []int, used []bool) {
+func dfs(level int, nums []int, res *[][]int) {
+	if level == len(nums) {
+		*res = append(*res, append([]int(nil), nums...))
+
+		return
+	}
+
+	// 需要从 level层开始
+	for i := level; i < len(nums); i++ {
+		// 交换值，其实就相当于确定 level 的位置，
+		// 而且一定不会重复了，因为都已经去重了嘛
+		swap(nums, level, i)
+		dfs(level+1, nums, res)
+		// 还原现场
+		swap(nums, level, i)
+	}
+}
+
+func swap(nums []int, i, j int) {
+	nums[i], nums[j] = nums[j], nums[i]
+}
+
+func Permute1(nums []int) [][]int {
+	if nums == nil {
+		return nil
+	}
+	l := len(nums)
+	track := make([]int, l)
+	used := make([]bool, l)
+	results := make([][]int, 0)
+	dfs1(0, &results, nums, track, used)
+
+	return results
+}
+
+func dfs1(level int, results *[][]int, nums, track []int, used []bool) {
 	if level == len(nums) {
 		// 说明到了最底层，记录结果
 		*results = append(*results, append([]int(nil), track...))
@@ -35,7 +68,7 @@ func dfs(level int, results *[][]int, nums, track []int, used []bool) {
 		track[level] = v
 		used[i] = true
 		// 往下一层钻
-		dfs(level+1, results, nums, track, used)
+		dfs1(level+1, results, nums, track, used)
 		// 还原现场
 		used[i] = false
 	}
