54ffbbe103ff6d27aaaafb59458ff7a14e2b8a7f
Ciusyan <1533460130@qq.com>
2023-07-31 16:06:45 +0800

    相同的树

---
+++ tree/相同的树.go
@@ -0,0 +1,20 @@
+// @Author: Ciusyan 2023/7/31
+
+package tree
+
+// https://leetcode.cn/problems/same-tree/
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if (p == nil && q != nil) || (p != nil && q == nil) {
+		// 说明有一个是空树，肯定不相等
+		return false
+	}
+
+	if p == nil && q == nil {
+		// 说明两个都是空树，肯定相等
+		return true
+	}
+
+	// 判断当前节点是否相等 && 左子树是否相等 && 右子树是否相等
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
