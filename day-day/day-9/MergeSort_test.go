4b184cdea6b472ffa50e21bf5e6af8e5e1a01487
ciusyan <1533460130@qq.com>
2023-09-14 08:39:38 +0800

    递归实现归并排序

---
+++ day-day/day-9/MergeSort_test.go
@@ -0,0 +1,14 @@
+// @Author: Ciusyan 9/14/23
+
+package day_9
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMergeSort1(t *testing.T) {
+	arr := []int{2, 4, 1, 6, 2, 1, 3, 8, 5, 6, 3, 10}
+	MergeSort1(arr)
+	fmt.Println(arr)
+}
---
+++ day-day/day-9/归并排序.go
@@ -0,0 +1,74 @@
+// @Author: Ciusyan 9/14/23
+
+package day_9
+
+// MergeSort1 归并排序
+//
+//	递归版实现
+func MergeSort1(arr []int) {
+	if arr == nil || len(arr) < 2 {
+		return
+	}
+
+	sort(arr, 0, len(arr))
+}
+
+// 对 arr 中 [begin, end) 排序
+func sort(arr []int, begin, end int) {
+	// 如果只有一个元素了，就没必要排序了
+	if end-begin < 2 {
+		return
+	}
+
+	// 求出中点位置
+	mid := begin + (end-begin)>>1
+	// 对左右分别进行归并排序
+	sort(arr, begin, mid)
+	sort(arr, mid, end)
+	// 排序完成后，对左右两边进行合并
+	merge(arr, begin, mid, end)
+}
+
+func merge(arr []int, begin, mid, end int) {
+	// 准备一个大数组
+	tempArr := make([]int, end-begin)
+	i := 0
+	// 准备左右指针
+	p1 := begin
+	p2 := mid
+
+	for p1 < mid && p2 < end {
+		// 比较，谁小放谁，默认 p1 位置的小
+		temp := arr[p1]
+		p1++
+
+		if arr[p2] < temp {
+			// 说明当初错付了，选择对的，别忘了还回旧的
+			temp = arr[p2]
+			p1--
+			p2++
+		}
+
+		// 小的给到 i 位置
+		tempArr[i] = temp
+		i++
+	}
+	// 来到这里，要么 p1 越界了，要么 p2 越界了
+	for p1 < mid {
+		tempArr[i] = arr[p1]
+		p1++
+		i++
+	}
+
+	for p2 < end {
+		tempArr[i] = arr[p2]
+		p2++
+		i++
+	}
+
+	// 将旧数组改写
+	for _, v := range tempArr {
+		arr[begin] = v
+		begin++
+	}
+}
