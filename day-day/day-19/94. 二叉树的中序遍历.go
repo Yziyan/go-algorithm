5e9b63b15e178f4f6c6d38645ae017e2964a11e2
ciusyan <1533460130@qq.com>
2023-10-05 17:00:30 +0800

    中序遍历，非递归实现

--- day-day/day-19/94. 二叉树的中序遍历.go
+++ day-day/day-19/94. 二叉树的中序遍历.go
@@ -4,8 +4,40 @@ package day_19
 
 // https://leetcode.cn/problems/binary-tree-inorder-traversal/description/
 
-// 递归实现版本
+// 非递归实现
 func inorderTraversal(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+
+	// 准备一个栈
+	stack := NewStack()
+	// 准备一个用于遍历的节点
+	cur := root
+
+	// 只要有一个不为 nil，就别停
+	for cur != nil || stack.Size() != 0 {
+		if cur != nil {
+			// 我们需要一路向左，所以需要记录路径
+			stack.Push(cur)
+			// 然后向左走
+			cur = cur.Left
+		} else {
+			// 来到这里，说明已经到了某一次的最左侧，需要弹出来访问
+			node := stack.Pop()
+			// 搞事情
+			res = append(res, node.Val)
+			// 然后将当前访问过的元素的右子节点当做根节点，看看它能否往下钻
+			cur = node.Right
+		}
+	}
+
+	return res
+}
+
+// 递归实现版本
+func inorderTraversal1(root *TreeNode) []int {
 	res := make([]int, 0)
 	inorder(root, &res)
 
