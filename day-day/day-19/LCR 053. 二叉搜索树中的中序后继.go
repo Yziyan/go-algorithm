1de78ef9ff254aa23e3dadb592983eab399e5998
ciusyan <1533460130@qq.com>
2023-10-12 08:16:05 +0800

    二叉树中序遍历的后继节点(二叉搜索树的性质)

--- day-day/day-19/LCR 053. 二叉搜索树中的中序后继.go
+++ day-day/day-19/LCR 053. 二叉搜索树中的中序后继.go
@@ -4,8 +4,47 @@ package day_19
 
 // https://leetcode.cn/problems/P5rCT8/
 
-// 中序遍历法
+// 二叉树性质法
 func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
+	if root == nil || p == nil {
+		return nil
+	}
+
+	// 如果 p 节点有右子树，那么后继节点一定右子树上。
+	succ := p.Right
+	if succ != nil {
+		// 说明有右子树，那么后继节点就存在于右子树的最左边
+		for succ.Left != nil {
+			succ = succ.Left
+		}
+
+		// 来到这里，肯定找到了后继
+		return succ
+	}
+
+	// 来到这里，存在两种情况，1. 不存在后继 2. 存在于最祖先节点
+	// 但是我们都可以统统从根节点往下遍历：
+
+	cur := root
+	for cur != nil {
+		if p.Val > cur.Val {
+			// 说明在右边还没找到比 p 大的，肯定还不可能是后继
+			cur = cur.Right
+		} else if p.Val == cur.Val {
+			// 来到这，说明肯定找到了后继，可以不用遍历至末尾了
+			break
+		} else {
+			// 说明有比 p 还大的了，可能是后继。
+			succ = cur
+			cur = cur.Left
+		}
+	}
+	// 遍历完一条
+	return succ
+}
+
+// 中序遍历法
+func inorderSuccessor1(root *TreeNode, p *TreeNode) *TreeNode {
 	if root == nil || p == nil {
 		return nil
 	}
