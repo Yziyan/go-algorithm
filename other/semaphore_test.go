36d5ff1a914861902402279dfcad49d2f18516ff
ciusyan <1533460130@qq.com>
2023-10-26 21:27:06 +0800

    信号量锁

--- other/semaphore_test.go
+++ other/semaphore_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"golang.org/x/sync/semaphore"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -49,4 +50,64 @@ func TestSemaphore(t *testing.T) {
 
 	// 等待所有协程完成
 	wg.Wait()
+}
+
+var maxConcurrent int32 = 1
+
+func TestSemaphore2(t *testing.T) {
+
+	// 使用信号量锁
+	sem := semaphore.NewWeighted(int64(atomic.LoadInt32(&maxConcurrent)))
+
+	wg := sync.WaitGroup{}
+	// ctx := context.Background()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		updateMaxConcurrent(5)
+	}()
+
+	for i := 1; i <= 10; i++ {
+
+		wg.Add(1)
+
+		go func(taskId int) {
+			// 别忘了释放资源
+			defer wg.Done()
+
+			if !tryAcquireWithMaxConcurrent(sem) {
+				// 没获取到锁
+				t.Logf("TaskId = %d 尚未获取到锁", taskId)
+				return
+			}
+			// 这里说明成功获取锁了，退出时记得释放
+			defer sem.Release(1)
+
+			// 执行任务
+			t.Logf("TaskId = %d 正在执行中", taskId)
+			time.Sleep(2 * time.Second)
+
+		}(i)
+
+		time.Sleep(1 * time.Second)
+	}
+
+	wg.Wait()
+}
+
+func tryAcquireWithMaxConcurrent(sem *semaphore.Weighted) bool {
+	max := atomic.LoadInt32(&maxConcurrent)
+	if max <= 0 {
+		return false
+	}
+	if sem.TryAcquire(1) {
+		atomic.AddInt32(&maxConcurrent, -1)
+		return true
+	}
+	return false
+}
+
+// 在运行时更新 maxConcurrent 的值
+func updateMaxConcurrent(newMaxConcurrent int32) {
+	atomic.StoreInt32(&maxConcurrent, newMaxConcurrent)
 }
