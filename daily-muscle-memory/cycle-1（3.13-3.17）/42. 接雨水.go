4b45470a9a6d85c03969c8796516f71c5f5372ad
ciusyan <1533460130@qq.com>
2024-03-13 07:52:18 +0800

    42. 接雨水.go 思路重复+代码重复

--- daily-muscle-memory/cycle-1（3.13-3.17）/42. 接雨水.go
+++ daily-muscle-memory/cycle-1（3.13-3.17）/42. 接雨水.go
@@ -44,3 +44,55 @@ func trap(height []int) int {
 
 	return water
 }
+
+// 思路重复 + 代码重复
+/*
+1.核心就是：一根柱子一根柱子的计算，即竖着看能接的雨水量
+2.能接雨水的柱子，它的左右两边一定有比它高的柱子。
+3.所以准备两个指针，left 和 right，每次都找出他们最低的一边 lower。
+4.那么就只用考虑 lower 所在边即可，另一遍一定比它高
+5.所以又准备了一个不断更新的 lowerMax，这个只可能被每一次 lower 所在边更新。
+	5.1 要么原本的值本来就在 lower 所在边，因为每次只有较低的柱子才会往中心挪动
+	5.2 要么此次会使用 lower 更新 lowerMax，那么也会变成 lower 所在边
+6. 所以此次能接住的雨水，就是 lowerMax-lower
+
+// 伪代码
+1.判空
+2.准备 water 和 lowerMax，还有双指针 left 和 right
+3.进行遍历，直至两个指针相遇
+	3.1 找出 lower，并且低的一边，要往中心靠
+	3.2 尝试更新 lowerMax
+	3.3 添加上此次接的雨水量
+4.返回water
+*/
+func trap2(height []int) int {
+	if height == nil || len(height) == 0 {
+		return 0
+	}
+
+	var (
+		water    = 0
+		lowerMax = 0
+
+		left  = 0
+		right = len(height) - 1
+	)
+
+	for left < right {
+		lower := height[left]
+		left++
+		if height[right] < lower {
+			lower = height[right]
+			right--
+			left--
+		}
+
+		if lower > lowerMax {
+			lowerMax = lower
+		}
+
+		water += lowerMax - lower
+	}
+
+	return water
+}
