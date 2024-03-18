61d994db990a535065e810c2010ed893f7cab283
ciusyan <1533460130@qq.com>
2024-03-18 19:57:43 +0800

    236. 二叉树的最近公共祖先.go(思路重复，代码重复)

--- daily-muscle-memory/cycle-1-3.18-3.22/236. 二叉树的最近公共祖先.go
+++ daily-muscle-memory/cycle-1-3.18-3.22/236. 二叉树的最近公共祖先.go
@@ -30,3 +30,43 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	}
 	return left
 }
+
+/*
+*
+思路重复
+对于给定的 p 和 q，去找它存在这颗树的公共祖先在什么地方，无非就这几种情况：
+1. 在左子树（p 和 q 都在左子树）
+2. 在右子树（p 和 q 都在右子树）
+3. 在根节点（p 和 q 分布在左右）
+4. 不存在 （p 或 q 不存在树上）
+
+那么也就可以进行递归求解了，明确递归含义：
+在 root 上，查找出 p 和 q 的最近公共祖先。
+为什么呢？因为对于递归基的设定。是：
+如果 root 为 nil 了，肯定得不到结果，如果 p 或 q 其中一个是 root，那么他们最近的公共祖先肯定是 root 了，
+
+	因为它是此次调用中，最高的节点了，已经到 root 了
+
+那么对于上面四种情况：
+1.递归去左子树，肯定能得到结果，并且递归去右子树，一定不存在结果。
+2.递归去右子树，肯定能得到结果，并且递归去左子树，一定不存在结果。
+3.递归去左子树，能够得到结果，并且递归去右子树，也能得到结果。
+4.去左右都获取不到结果
+*/
+func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
+	if root == nil || p == root || q == root {
+		return root
+	}
+
+	left := lowestCommonAncestor2(root.Left, p, q)
+	right := lowestCommonAncestor2(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left == nil {
+		return right
+	}
+	return left
+}
