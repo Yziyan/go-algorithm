1fb472c1af3e0aa26afc347d8bbbc923135efbd6
ciusyan <1533460130@qq.com>
2024-06-04 07:32:24 +0800

    100.same tree

---
+++ daily-muscle-memory/cycle-15-6.1-6.5/100. 相同的树.go
@@ -0,0 +1,21 @@
+// @Author: Ciusyan 6/4/24
+
+package cycle_14_5_27_5_31
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil || q == nil {
+		return false
+	}
+
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
