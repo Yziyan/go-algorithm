6d66ef7f15c3b88e430b23bc31390d9ce3d34940
ciusyan <1533460130@qq.com>
2024-07-03 12:27:15 +0800

    merge: 合并分支

--- daily-muscle-memory/cycle-18-7.1-7.5/5. 最长回文子串.go
+++ daily-muscle-memory/cycle-18-7.1-7.5/5. 最长回文子串.go
@@ -4,6 +4,18 @@ package cycle_18_7_1_7_5
 
 // https://leetcode.cn/problems/longest-palindromic-substring/
 
+/**
+思路重复：
+我们首先找到这个字符串，有多少回文子串。
+然后我们对所有的回文子串的长度进行比较，找出最大的那个即可。
+我们可以使用 dp 的方式，来进行判断，是否是回文子串。
+即当 dp[row][col] 代表着 s[row ... col] 是否是回文子串。
+那么 dp[row][row] 肯定是回文子串，因为 s[row] == s[row]，这就是初始条件
+那么一般情况呢？当 col-row+1 <= 2 时，相当于最多只有两个端点，那么如果 s[row] == s[col]，即是回文子串。
+否则首先看端点，然后看看 dp[row+1][col-1]，这个去掉端点后的是不是回文子串，如果是，才能说明整体是回文子串。
+
+当计算出一个回文子串后，就看看能不能更新结果的长度？还有回文串结果的起始点。
+*/
 func longestPalindrome(s string) string {
 	n := len(s)
 	// dp[i][j] 代表，s [i ... j] 是回文子串
