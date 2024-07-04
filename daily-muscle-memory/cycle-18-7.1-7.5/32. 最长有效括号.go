de02e74ded710eee99ca223ee1a11a759de2d863
ciusyan <1533460130@qq.com>
2024-07-04 12:56:21 +0800

    merge: 合并分支

--- daily-muscle-memory/cycle-18-7.1-7.5/32. 最长有效括号.go
+++ daily-muscle-memory/cycle-18-7.1-7.5/32. 最长有效括号.go
@@ -4,6 +4,18 @@ package cycle_18_7_1_7_5
 
 // https://leetcode.cn/problems/longest-valid-parentheses/
 
+/**
+思路重复：
+使用 Dp 来实现，
+dp[i] 代表，以 s[i] 结尾的字符的最长有效括号的长度。
+那么当 s[i] == "("，肯定不可能是有效的括号对。直接跳过即可
+只有当 s[i] == ")" 的时候，才有可能有有效的括号对。那么长度是多少呢？可以往前找到一个合法的与之配对的 pre 左括号。
+pre = i - 1 - dp[i-1]， dp[i-1] 代表前一个字符的有效括号对长度，
+那么当 s[pre] 是 "(" 的时候，才能是一个有效括号对。
+所以：dp[i] = 2 + dp[i-1]，2 是 s[i] 和 s[pre]，dp[i-1] 还得看前一个字符的有效括号对
+但是还有一种情况，如果 pre > 0 的，那么 pre 前面也可能有有效的括号对，那么 dp[i] 还得加上 dp[pre+1]
+*/
+
 func longestValidParentheses(s string) int {
 	n := len(s)
 	// dp[i] 代表，s[i] 结尾的字符串的最长有效括号长度
