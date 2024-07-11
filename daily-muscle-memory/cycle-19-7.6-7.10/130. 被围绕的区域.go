db3cf8d18284c182d707b578eaa82b224f46055e
ciusyan <1533460130@qq.com>
2024-07-11 17:05:33 +0800

    merge

--- daily-muscle-memory/cycle-19-7.6-7.10/130. 被围绕的区域.go
+++ daily-muscle-memory/cycle-19-7.6-7.10/130. 被围绕的区域.go
@@ -4,6 +4,18 @@ package cycle_19_7_6_7_10
 
 // https://leetcode.cn/problems/surrounded-regions/description/
 
+/*
+*
+思路重复：
+可以使用感染的方式来解答。
+1.先处理边缘，如果与边缘联通的 O，我们将其感染变成 T。
+2.然后再全部遍历，当再遇到 O 时，说明是被包围的，将其变成 X，如果遇到了 T，说明是边缘联通的，将其变回 O
+
+那么如何进行感染呢？其实也不难。
+先对当前位置进行修改，修改完成后，递归的去上下左右四个方向进行感染。
+当 row or col 越界的时候，或者当前字符并不是需要转化的字符，就可以将其返回了
+*/
+
 func solve(board [][]byte) {
 	const (
 		DIC = 'X'
