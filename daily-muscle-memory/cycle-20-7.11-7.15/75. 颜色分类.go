8b316f3ebab12b55b2978e5ace2d011ae70a359c
ciusyan <1533460130@qq.com>
2024-07-11 17:10:11 +0800

    merge

--- daily-muscle-memory/cycle-20-7.11-7.15/75. 颜色分类.go
+++ daily-muscle-memory/cycle-20-7.11-7.15/75. 颜色分类.go
@@ -4,7 +4,17 @@ package cycle_20_7_11_7_15
 
 // https://leetcode.cn/problems/sort-colors/
 
-func sortColors(nums []int) {
+/**
+思路重复：
+经典的荷兰国旗问题。
+准备三指针：l, r, cur
+用 cur 从左往右扫描，
+* 遇到比 1 小的，就和 l 交换，并且 cur 和 l 都加一
+* 遇到和 1 相等的，cur 往后加一
+* 遇到比 1 大的，就和 r 交换，并且 r 往前减一
+*/
+
+func sortColors1(nums []int) {
 	l, r := 0, len(nums)-1
 
 	for cur := 0; cur <= r; {
