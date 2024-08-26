7641a763c31fa8c560d5bf53dd9619dfe14c74a6
ciusyan <1533460130@qq.com>
2024-08-26 12:59:12 +0800

    merge

--- daily-muscle-memory/cycle-27-8.25-8.29/22. 括号生成.go
+++ daily-muscle-memory/cycle-27-8.25-8.29/22. 括号生成.go
@@ -1,8 +1,19 @@
 // @Author: Ciusyan 2024/8/26
 
-package cycle_26_8_25_8_29
+package cycle_27_8_25_8_29
 
 // https://leetcode.cn/problems/generate-parentheses/description/
+
+/*
+*
+思路重复：
+使用 DFS 的方式即可，总共有 n 对括号，那么一个合法的字符串就是 2n 的长度。
+那么我们从第 0 层开始，去收集括号，直至到达 2n 的长度后，就将其轨迹放入结果中，
+那么如何收集呢？
+只要有左括号，就可以进行收集，记录轨迹后去下一层搜索，并且将做括号余量减一
+对于右括号，则需要比做括号多，才能够选择，否则选了也是无效括号，
+循环往复，直至全部收集完毕，
+*/
 
 func generateParenthesis(n int) []string {
 
